8f4a94f3295ab7cea5349bff0c257fb39c3756c1
yuyong_id <437112352@qq.com>
2020-07-14 16:50:24 +0800

    [chore] modify deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,26 +1,25 @@
-#!/bin/bash
+#!/bin/sh
 
-if [ "`git status -s`" ]
-then
-      echo "The working directory is dirty. Please commit any pending changes."
-          exit 1;
+# If a command fails then the deploy stops
+set -e
+
+printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
+
+# Build the project.
+hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
+
+# Go To Public folder
+cd public
+
+# Add changes to git.
+git add .
+
+# Commit changes.
+msg="rebuilding site $(date)"
+if [ -n "$*" ]; then
+	msg="$*"
 fi
+git commit -m "$msg"
 
-echo "Deleting old publication"
-rm -rf public
-mkdir public
-git worktree prune
-rm -rf .git/worktrees/public/
-
-echo "Checking out gh-pages branch into public"
-git worktree add -B gh-pages public upstream/gh-pages
-
-echo "Removing existiggng files"
-rm -rf public/*
-
-echo "Generating site"
-hugo
-docker run --rm -it -v 
-
-echo "Updating gh-pages branch"
-cd public && git add --all && git commit -m 
+# Push source and build repos.
+git push origin master
